627eb2a0dafcb1c5578020d60ea5f3568450d05e
洛水赋神 <2248064246@qq.com>
2021-07-30 18:40:13 +0800

    流

--- JS/JS-API/流/测试/流生成和消费/text2.js
+++ JS/JS-API/流/测试/流生成和消费/text2.js
@@ -2,9 +2,45 @@
  * @Author: ys4225/黄迎李
  * @Date: 2021-07-30 16:19:36
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-07-30 16:19:37
+ * @LastEditTime: 2021-07-30 18:33:52
  * @Description: 
  */
 
+let interval, i = 0
 
-let reader = new ReadableStream()
+let writeStream = new WritableStream({
+  start(controller) {
+
+  },
+  write(chunk, controller) {
+    console.log(chunk)
+  }
+})
+
+let writer = writeStream.getWriter()
+
+
+
+interval = setInterval(() => {
+  console.log('i', i)
+  if (++i > 3) {
+    clearInterval(interval)
+    writer.close()
+    writer.releaseLock()
+  } else {
+    writer.write(Math.floor(Math.random() * 10))
+  }
+}, 1000)
+
+
+let readStream = new ReadableStream(
+)
+
+
+let reader = readStream.getReader()
+
+let read = reader.read()
+
+read.then(data => {
+  console.log(data)
+})
