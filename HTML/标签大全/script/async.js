19190c171c393e7931d2a105bb08919851655210
ggbone <2248064246@qq.comj>
2021-12-29 01:18:20 +0800

    script

---
+++ HTML/标签大全/script/async.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: huangyingli
+ * @Date: 2021-12-29 00:16:17
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-12-29 00:16:17
+ * @Description: 
+ */
+console.log('这是async脚本')
---
+++ HTML/标签大全/script/defer.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: huangyingli
+ * @Date: 2021-12-29 00:11:43
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-12-29 00:11:44
+ * @Description: 
+ */
+
+console.log('这是defer脚本_1')
