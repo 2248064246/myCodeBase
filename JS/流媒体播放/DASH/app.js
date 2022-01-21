1877288aee742940058bfa3756d7ff915f936b62
洛水赋神 <2248064246@qq.com>
2022-01-21 15:31:36 +0800

    流媒体 DASH

--- JS/流媒体播放/DASH/app.js
+++ JS/流媒体播放/DASH/app.js
@@ -1,9 +1,9 @@
 /*
  * @Author: huangyingli
- * @Date: 2022-01-20 17:12:16
+ * @Date: 2022-01-21 15:12:24
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-01-20 20:26:03
- * @Description: 
+ * @LastEditTime: 2022-01-21 15:13:37
+ * @Description:
  */
 const NodeMediaServer = require('node-media-server');
 
@@ -13,12 +13,12 @@ const config = {
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
-    ping_timeout: 60
+    ping_timeout: 60,
   },
   http: {
     port: 8000,
     mediaroot: './media',
-    allow_origin: '*'
+    allow_origin: '*',
   },
   trans: {
     ffmpeg: 'D:/ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe',
@@ -28,11 +28,11 @@ const config = {
         hls: true,
         hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
         dash: true,
-        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
-      }
-    ]
-  }
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]',
+      },
+    ],
+  },
 };
 
-var nms = new NodeMediaServer(config)
+var nms = new NodeMediaServer(config);
 nms.run();
