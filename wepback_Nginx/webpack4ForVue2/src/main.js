3de4e3f40e2cc8cf93e7b3f7d102d9dfda87fb72
ggbone <2248064246@qq.comj>
2022-02-19 17:55:43 +0800

    命名更改

--- wepback_Nginx/webpack4ForVue2/src/main.js
+++ wepback_Nginx/webpack4ForVue2/src/main.js
@@ -2,10 +2,14 @@
  * @Author: huangyingli
  * @Date: 2022-02-19 14:59:58
  * @LastEditors: huangyingli
- * @LastEditTime: 2022-02-19 16:44:14
+ * @LastEditTime: 2022-02-19 17:52:46
  * @Description:
  */
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-console.log('hell webpack')
+console.log('hell webpack');
+
+let a = [[1, [1, 3, 4, 5, [0]]]];
+
+console.log(a.flat(Infinity));
