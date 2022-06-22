5fa4aae2028a37167c8c6df69414a32d152736d2
洛水赋神 <2248064246@qq.com>
2022-06-22 17:44:01 +0800

    函数式编程

---
+++ JS/JS-API/数据类型/Function/Function-API.md
@@ -0,0 +1,11 @@
+# Function API
+
+## 静态方法和属性
+
+
+## 原型方法和属性
+
++ length 用于获取形参个数
++ apply
++ bind
++ call
---
+++ 书籍阅读/函数式编程/curry.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: huangyingli
+ * @Date: 2022-06-22 17:14:52
+ * @LastEditors: huangyingli
+ * @LastEditTime: 2022-06-22 17:42:38
+ * @Description: 
+ */
+
+
+function curry(fnc) {
+  if(typeof fnc !== 'function') {
+    throw Typed
+  }
+  let paramsLen = fnc.length;
+  return function (...argsOne) {
+
+  }
+}
