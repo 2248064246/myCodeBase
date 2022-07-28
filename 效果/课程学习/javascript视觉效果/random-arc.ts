c02b1d734936d6ff9f0fb862eaf1d12e1dc98fb4
洛水赋神 <2248064246@qq.com>
2022-07-28 15:11:52 +0800

    视觉效果

--- 效果/课程学习/javascript视觉效果/random-arc.ts
+++ 效果/课程学习/javascript视觉效果/random-arc.ts
@@ -4,7 +4,7 @@ import { cwd } from 'process';
  * @Author: huangyingli
  * @Date: 2022-07-27 22:19:42
  * @LastEditors: huangyingli
- * @LastEditTime: 2022-07-27 23:45:06
+ * @LastEditTime: 2022-07-28 14:58:19
  * @Description:
  */
 const canvasSketch = require('canvas-sketch');
@@ -16,7 +16,7 @@ type MCanvas = {
 };
 
 const settings = {
-  dimensions: [760, 800],
+  dimensions: [800, 800],
 };
 
 const sketch = () => {
@@ -28,6 +28,18 @@ function angleToRadian(angle: number) {
   return (angle / 180) * Math.PI;
 }
 
+function range(start: number, end: number) {
+  return Math.random() * (end - start) + start;
+}
+
+/**
+ * 整体思路就是在圆上画长条
+ * 让后随机宽高
+ * 再给每个对应长条对应 圆弧
+ * 随机半径, 弧长
+ * 效果很奶思
+ */
+
 function draw(canvas: MCanvas) {
   const { context, width, height } = canvas;
   /* center point */
@@ -38,17 +50,14 @@ function draw(canvas: MCanvas) {
   const h = width * 0.1;
   const r = width * 0.2;
 
-  const amount = 12;
+  const amount = 40;
 
   context.fillStyle = 'black';
   context.translate(cx, cy);
-  context.beginPath();
-  context.arc(0, 0, 3, 0, Math.PI * 2);
-  context.fill();
-  context.closePath();
 
   for (let i = 0; i < amount; i++) {
-    let radian = angleToRadian((360 / amount) * i);
+    let slice = angleToRadian(360 / amount);
+    let radian = slice * i;
 
     let x = r * Math.sin(radian);
     let y = r * Math.cos(radian);
@@ -56,9 +65,17 @@ function draw(canvas: MCanvas) {
     context.translate(x, y);
     context.rotate(-radian);
     context.beginPath();
+    context.scale(range(0.1, 2), range(0.2, 0.5));
     context.rect(-w / 2, -h / 2, w, h);
     context.fill();
+    context.restore();
 
+    context.save();
+    context.beginPath();
+    context.lineWidth = range(5, 15)
+    context.rotate(-radian);
+    context.arc(0, 0, r * range(0.7, 1.7), slice * range(-8, 0), slice * range(0, 5));
+    context.stroke();
     context.restore();
   }
 }
